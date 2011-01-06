942cac3bfc20ec9675355763dda79d7b40cf5f75
Benjamin Ross <benr22@gmail.com>
2011-01-06 07:27:25 +0000

    This is all of the given code by Mackey

---
+++ asg1/inode.cc
@@ -0,0 +1,90 @@
+// $Id: inode.cc,v 1.5 2010-12-13 20:11:09-08 - - $
+
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+#include "trace.h"
+#include "inode.h"
+
+int inode::next_inode_nr = 1;
+
+inode::inode(inode_t init_type):
+   inode_nr (next_inode_nr++), type (init_type)
+{
+   switch (type) {
+      case DIR_INODE:
+           contents.dirents = new directory();
+           break;
+      case FILE_INODE:
+           contents.data = new wordvec();
+           break;
+   }
+   TRACE ('i', "inode " << inode_nr << ", type = " << type);
+}
+
+//
+// copy ctor -
+//    Make a copy of a given inode.  This should not be used in
+//    your program if you can avoid it, since it is expensive.
+//    Here, we can leverage operator=.
+//
+inode::inode (const inode &that) {
+   *this = that;
+}
+
+//
+// operator= -
+//    Assignment operator.  Copy an inode.  Make a copy of a
+//    given inode.  This should not be used in your program if
+//    you can avoid it, since it is expensive.
+//
+inode &inode::operator= (const inode &that) {
+   if (this != &that) {
+      inode_nr = that.inode_nr;
+      type = that.type;
+      contents = that.contents;
+   }
+   TRACE ('i', "inode " << inode_nr << ", type = " << type);
+   return *this;
+}
+
+int inode::get_inode_nr() {
+   TRACE ('i', "inode = " << inode_nr);
+   return inode_nr;
+}
+
+int inode::size() {
+   int size = 0;
+   TRACE ('i', "size = " << size);
+   return size;
+}
+
+const wordvec &inode::readfile() const {
+   TRACE ('i', *contents.data);
+   assert (type == FILE_INODE);
+   return *contents.data;
+}
+
+void inode::writefile (const wordvec &words) {
+   TRACE ('i', words);
+   assert (type == FILE_INODE);
+}
+
+void inode::remove (const string &filename) {
+   TRACE ('i', filename);
+   assert (type == DIR_INODE);
+}
+
+inode_state::inode_state(): root (NULL), cwd (NULL), prompt ("%") {
+   TRACE ('i', "root = " << (void*) root << ", cwd = " << (void*) cwd
+          << ", prompt = " << prompt);
+}
+
+ostream &operator<< (ostream &out, const inode_state &state) {
+   out << "inode_state: root = " << state.root
+       << ", cwd = " << state.cwd;
+   return out;
+}
+
---
+++ asg1/yshell.cc
@@ -0,0 +1,92 @@
+// $Id: yshell.cc,v 1.2 2010-12-13 20:11:09-08 - - $
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <unistd.h>
+
+using namespace std;
+
+#include "commands.h"
+#include "trace.h"
+#include "inode.h"
+#include "util.h"
+
+//
+// scan_options
+//    Options analysis:  The only option is -Dflags. 
+//
+
+void scan_options (int argc, char **argv) {
+   opterr = 0;
+   for (;;) {
+      int option = getopt (argc, argv, "@:");
+      if (option == EOF) break;
+      switch (option) {
+         case '@':
+            traceflags::setflags (optarg);
+            break;
+         default:
+            complain() << "-" << (char) option << ": invalid option"
+                       << endl;
+            break;
+      }
+   }
+   if (optind < argc) {
+      complain() << "operands not permitted" << endl;
+   }
+}
+
+//
+// main -
+//    Main program which loops reading commands until end of file.
+//
+
+int main (int argc, char **argv) {
+   setexecname (argv[0]);
+   cout << boolalpha; // Print false or true instead of 0 or 1.
+   cerr << boolalpha;
+   scan_options (argc, argv);
+   bool need_echo = want_echo();
+   commands cmdmap;
+   string prompt = "%";
+   inode_state state;
+   try {
+      for (;;) {
+         try {
+   
+            // Read a line, break at EOF, and echo print the prompt
+            // if one is needed.
+            cout << prompt << " ";
+            string line;
+            getline (cin, line);
+            if (cin.eof()) {
+               if (need_echo) cout << "^D";
+               cout << endl;
+               TRACE ('y', "EOF");
+               break;
+            }
+            if (need_echo) cout << line << endl;
+   
+            // Split the line into words and lookup the appropriate
+            // function.  Complain or call it.
+            wordvec words = split (line, " \t");
+            TRACE ('y', "words = " << words);
+            function fn = cmdmap[words[0]];
+            if (fn == NULL) {
+               throw yshell_exn (words[0] + ": no such function");
+            }
+            fn (state, words);
+         }catch (yshell_exn exn) {
+            // If there is a problem discovered in any function, an
+            // exn is thrown and printed here.
+            complain() << exn.what() << endl;
+         }
+      }
+   } catch (ysh_exit_exn) {
+   }
+
+   return exit_status_message();
+}
+
